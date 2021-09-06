dc2f91cd52259a331055598ae9496aeb0bfb32e6
Vjacheslav Borisov <slavb@ilb.ru>
2021-09-06 16:00:40 +0400

    node-fetch-with-proxy test

--- test/UriAccessorHttp.test.js
+++ test/UriAccessorHttp.test.js
@@ -2,6 +2,7 @@ import UriAccessorHttp from '../src/UriAccessorHttp';
 import finalhandler from 'finalhandler';
 import http from 'http';
 import serveStatic from 'serve-static';
+import * as node_fetch_with_proxy from 'node-fetch-with-proxy';
 
 const serve = serveStatic('test/data', { index: ['index.html', 'index.htm'] });
 const server = http.createServer(function onRequest(req, res) {
@@ -16,19 +17,32 @@ afterAll(async () => {
   server.close();
 });
 
-const uri = 'http://localhost:3030/testfile.txt';
-
-const expectedStr = 'test content';
-const expectedBuf = Buffer.from(expectedStr, 'utf8');
-
 test('getContent', async () => {
+  const uri = 'http://localhost:3030/testfile.txt';
+  const expectedStr = 'test content';
   const uriAccessor = new UriAccessorHttp(uri);
   const content = await uriAccessor.getContent();
   expect(content).toStrictEqual(expectedStr);
 });
 
 test('getBinary', async () => {
+  const uri = 'http://localhost:3030/testfile.txt';
+  const expectedStr = 'test content';
+  const expectedBuf = Buffer.from(expectedStr, 'utf8');
+
   const uriAccessor = new UriAccessorHttp(uri);
   const binary = await uriAccessor.getBinary();
   expect(binary).toStrictEqual(expectedBuf);
+});
+
+const maybe = process.env.https_proxy ? describe : describe.skip;
+maybe('proxy test', () => {
+  test('node-fetch-with-proxy test', async () => {
+    const uri = 'https://github.com/ilb/uriaccessorjs/raw/master/test/data/testfile.txt';
+    const expectedStr = 'test content';
+
+    const uriAccessor = new UriAccessorHttp(uri, null, node_fetch_with_proxy);
+    const content = await uriAccessor.getContent();
+    expect(content).toStrictEqual(expectedStr);
+  });
 });
