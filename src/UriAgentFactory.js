9b54ed6aaf563385a1d924a3c80e4e5ee3e8c6b5
Vjacheslav Borisov <slavb@ilb.ru>
2021-10-07 10:11:23 +0400

    Match uri by String (startsWith) or RegExp (match) key

--- src/UriAgentFactory.js
+++ src/UriAgentFactory.js
@@ -1,12 +1,27 @@
+/**
+ * Match uri by String (startsWith) or RegExp (match) key
+ * @param {String | RegExp} key
+ * @param {String} uri
+ * @returns
+ */
+function matchUri(key, uri) {
+  if (key instanceof RegExp) {
+    return uri.match(key);
+  } else {
+    return uri.startsWith(key);
+  }
+}
+
 export default class UriAgentFactory {
   constructor({ uriAgentMap }) {
     this.map = uriAgentMap || null;
   }
+
   /*eslint no-unused-vars: ["error", { "args": "none" }]*/
   getAgent(uri) {
     if (this.map) {
       for (const [key, value] of this.map.entries()) {
-        if (uri.match(key)) {
+        if (matchUri(key, uri)) {
           return value;
         }
       }
--- test/UriAgentFactory.test.js
+++ test/UriAgentFactory.test.js
@@ -1,10 +1,22 @@
 import UriAgentFactory from '../src/UriAgentFactory';
 import { configureAgent } from '../src/agentutils';
 
-test('get getAgent', async () => {
+test('getAgent_RegExp', async () => {
   const uriAgentMap = new Map();
   uriAgentMap.set(/https:\/\/www.google.com.*/, configureAgent('test/data/cert.pem', 'ajuij2Ec'));
   const uaf = new UriAgentFactory({ uriAgentMap });
   const agent = uaf.getAgent('https://www.google.com');
   expect(agent.options.passphrase).toStrictEqual('ajuij2Ec');
+  const agent2 = uaf.getAgent('https://google.com');
+  expect(agent2).toStrictEqual(null);
+});
+
+test('getAgent_String', async () => {
+  const uriAgentMap = new Map();
+  uriAgentMap.set('https://www.google.com', configureAgent('test/data/cert.pem', 'ajuij2Ec'));
+  const uaf = new UriAgentFactory({ uriAgentMap });
+  const agent = uaf.getAgent('https://www.google.com');
+  expect(agent.options.passphrase).toStrictEqual('ajuij2Ec');
+  const agent2 = uaf.getAgent('https://google.com');
+  expect(agent2).toStrictEqual(null);
 });
