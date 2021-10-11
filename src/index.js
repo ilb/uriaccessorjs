52a347538bf8680749348b00e61143bc310de68d
Vjacheslav Borisov <slavb@ilb.ru>
2021-10-11 14:17:45 +0400

    export UriAgentFactory

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ export { default as UriAccessor } from './UriAccessor.js';
 export { default as UriAccessorFile } from './UriAccessorFile.js';
 export { default as UriAccessorHttp } from './UriAccessorHttp.js';
 export { default as UriAccessorFactory } from './UriAccessorFactory.js';
+export { default as UriAgentFactory } from './UriAgentFactory.js';
 export { default as UriCopier } from './UriCopier.js';
 export {
   configureCert,
